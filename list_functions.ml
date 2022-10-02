552ea69f7e02f5611c24e554bcfdafdae0058bad
wardstoneX <kastanselimmert_07@hotmail.com>
2022-10-02 12:15:16 +0200

    sum_int

--- list_functions.ml
+++ list_functions.ml
@@ -2,3 +2,8 @@ let length l = let rec aux l acc = match l with
 |[] -> acc
 | x::xs -> aux xs (acc+1)
 in aux l 0;;
+
+let sum_int l = let rec aux l acc = match l with
+| [] -> acc
+| x::xs -> aux xs (acc + x)
+in aux l 0;; 
