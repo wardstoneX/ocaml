71bd894705ead23ed7789b737ab9f3a73ce16c48
wardstoneX <kastanselimmert_07@hotmail.com>
2022-10-02 12:39:38 +0200

    implement some methods

--- list_functions.ml
+++ list_functions.ml
@@ -7,3 +7,35 @@ let sum_int l = let rec aux l acc = match l with
 | [] -> acc
 | x::xs -> aux xs (acc + x)
 in aux l 0;; 
+
+let sum_float l = let rec aux l acc = match l with
+| [] -> acc
+| x::xs -> aux xs (acc +. x)
+in aux l 0.0;; 
+
+
+let prod_int l = let rec aux l acc = match l with
+| [] -> acc
+| x::xs -> aux xs (acc * x)
+in aux l 1;; 
+
+let map l f = let rec aux l f acc = match l with
+| [] -> acc
+| x::xs -> aux xs f ((f x)::acc)
+in aux l f [];;
+
+let fold_left l f = let rec aux l f acc = match l with 
+| [] -> acc
+| x::xs -> aux xs f (f acc)
+in aux l f 0;;
+
+let filter l p = let rec aux l p acc = match l with
+| [] -> acc
+| x::xs -> if (p x) then aux xs p (x::acc) else  aux xs p acc
+in aux l p [];;
+
+let partition l p = let rec aux l p acc = match l with
+| [] -> acc
+| x::xs -> if (p x) then aux xs p ((x:: (fst acc)), snd acc)
+ else aux xs p (fst acc, (x:: (snd acc)))
+in aux l p ([],[]);;
